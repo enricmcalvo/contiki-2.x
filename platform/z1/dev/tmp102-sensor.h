332b8344745d664e77aef13a0c2e9115d4b85f9b
Enric M. Calvo <enric.calvo@gmail.com>
2011-03-11 17:50:57 +0100

    On-going:integrating tmp102 sensor to SENSORS list

---
+++ platform/z1/dev/tmp102-sensor.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2011, Zolertia(TM) is a Trademark by Advancare,SL
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @(#)$Id: temperature-sensor.h,v 1.1 2010/08/25 19:34:07 nifi Exp $
+ */
+
+/**
+ * \file
+ *         Digital Temperature sensor header file.
+ * \author
+ *         Adam Dunkels <adam@sics.se>
+ *         Joakim Eriksson <joakime@sics.se>
+ *         Niclas Finne <nfi@sics.se>
+ *         Enric M. Calvo <ecalov@zolertia.com>
+ */
+
+#ifndef __TMP102_SENSOR_H__
+#define __TMP102_SENSOR_H__
+
+#include "lib/sensors.h"
+#include "dev/tmp102.h"
+
+extern const struct sensors_sensor tmp102_sensor;
+
+#define TMP102_SENSOR "TMP102"
+
+#endif /* __TMP102_SENSOR_H__ */
